07469c2ee0af161b84d12a2d97d4b6cfd4b0b063
santoshpalla27 <santoshpalla2002@gmail.com>
2025-12-31 00:06:33 +0530

    clear

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AppRoutes from './app/routes';
-import './index.css';
-
-function App() {
-  return <AppRoutes />;
-}
-
-export default App;
--- frontend/src/components/UploadZone/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function UploadZone() {
-    return <div>Upload Zone Component</div>;
-}
